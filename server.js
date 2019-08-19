0544e62e421e1c2b320f75be1bdb74f89d8ab697
ckopecky <kopecky12112@gmail.com>
2019-08-19 13:35:07 -0700

    Add ProjectController to global middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const server = express();
 const port = process.env.PORT || 4000;
+const projectController = require('./data/controllers/projectController');
+
 server.use(express.json());
+server.use('/api/projects', projectController);
 
 server.get('/', (req, res) => {
     res.send({Success: "sanity check..."})
